6e53fce5698e4699216e4b22c9ab2c7581e55685
unknown <andre.git101@gmail.com>
2020-10-16 18:27:49 -0300

    adicionando a pasta mobile e web

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps'
+import {Feather} from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito'
+
+import Routes from './src/routes'
+
+export default function App() {
+  const [ fontsloaded ] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if(!fontsloaded){
+    return null;
+  }
+
+  return ( 
+    <Routes/>
+  );
+}
+
